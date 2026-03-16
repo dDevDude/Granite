aac5351c1921545860a6468d929ba56574d6b7c1
Sahil Patil <sahil.patil@joshsoftware.com>
2026-03-16 15:34:51 +0530

    Added show page for a task

--- app/javascript/src/components/Tasks/Table/Row.jsx
+++ app/javascript/src/components/Tasks/Table/Row.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Tooltip } from "components/commons";
 import PropTypes from "prop-types";
 
-const Row = ({ data }) => (
+const Row = ({ data, showTask }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map(rowData => (
       <tr key={rowData.id}>
@@ -12,6 +12,11 @@ const Row = ({ data }) => (
             <span>{rowData.title}</span>
           </Tooltip>
         </td>
+        <td className="cursor-pointer px-6 py-4 text-right text-sm font-medium leading-5">
+          <a className="text-indigo-600" onClick={() => showTask(rowData.slug)}>
+            Show
+          </a>
+        </td>
       </tr>
     ))}
   </tbody>
@@ -19,6 +24,7 @@ const Row = ({ data }) => (
 
 Row.propTypes = {
   data: PropTypes.array.isRequired,
+  showTask: PropTypes.func,
 };
 
 export default Row;
